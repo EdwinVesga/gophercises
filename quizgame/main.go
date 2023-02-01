db2d16dfb5ff84ddfa9d7ea62da60a957073fe06
Edwin Vesga <evesga@Edwins-MacBook-Air.local>
2023-02-01 13:19:44 -0500

    updated scaffolding, added timer and quiz logic

---
+++ quizgame/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/EdwinVesga/gophercises/quizgame/quiz"
+)
+
+const defaultFileName string = "problems.csv"
+const defaultTimeLimit int = 30
+
+func main() {
+	fmt.Println("Quiz Game - Gophercise #1")
+
+	csvFileName := flag.String("file", defaultFileName, "Csv file name with 'question,response' format.")
+	quizTimeLimit := flag.Int("limit", defaultTimeLimit, "the time limit for the quiz in seconds.")
+
+	flag.Parse()
+
+	f, err := os.Open(*csvFileName)
+
+	if err != nil {
+		fmt.Printf("Failed to open CSV file: %s.", *csvFileName)
+	}
+
+	d := time.Second * time.Duration(*quizTimeLimit)
+
+	quiz.StartQuiz(f, d)
+}
---
+++ quizgame/quiz/quiz.go
@@ -0,0 +1,56 @@
+package quiz
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func StartQuiz(file *os.File, duration time.Duration) {
+	r := csv.NewReader(file)
+
+	var i, sCounter int
+
+	go quizDuration(duration, &i, &sCounter)
+
+	for i = 1; ; i++ {
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		question, response := record[0], record[1]
+
+		fmt.Printf("Question %d - %s: ", i, question)
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		scanner.Scan()
+		input := scanner.Text()
+
+		if input == response {
+			fmt.Println("Correct!")
+			sCounter++
+		} else {
+			fmt.Println("Incorrect!")
+		}
+	}
+}
+
+func quizDuration(d time.Duration, n, i *int) {
+	<-time.NewTimer(d).C
+	fmt.Println()
+	fmt.Println("You have reached the time limit!")
+	fmt.Println("-- Quiz Summary --")
+	fmt.Printf("Correct: %d, Incorrect: %d", *i, *n-*i)
+	os.Exit(0)
+}
